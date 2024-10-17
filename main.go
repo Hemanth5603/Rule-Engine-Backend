b6745e69911519d88a5f7da6e372a951ba84586e
Hemanth5603 <shemanth2003.vskp@gmail.com>
2024-10-17 23:05:15 +0530

    added cors

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/hemanth5603/RuleEngineBackend/config"
 	"github.com/hemanth5603/RuleEngineBackend/handlers"
 )
@@ -9,6 +10,10 @@ import (
 func main() {
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // You can specify specific origins here, e.g., "https://your-vercel-app.vercel.app"
+		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
+	}))
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Zeotap Assignmment !!")
 	})
